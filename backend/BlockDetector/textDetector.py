be1135ba974cf2126b08db13629fe06735f05b85
aman jaiswal <jaiswalaman815@gmail.com>
2020-08-02 17:56:11 +0530

    Add token Detector for our visual programming language

---
+++ backend/BlockDetector/textDetector.py
@@ -0,0 +1,90 @@
+
+import cv2 
+import pytesseract
+import matplotlib.pyplot as plt
+import numpy as np
+
+img = cv2.imread('print5.jpg')
+
+
+# get grayscale image
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+# noise removal
+def remove_noise(image):
+    return cv2.medianBlur(image,5)
+
+#thresholding
+def thresholding(image):
+    # threshold the image, setting all foreground pixels to
+    # 255 and all background pixels to 0
+    return cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+#dilation
+def dilate(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.dilate(image, kernel, iterations = 1)
+    
+#erosion
+def erode(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.erode(image, kernel, iterations = 1)
+
+#opening - erosion followed by dilation
+def opening(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+
+#canny edge detection
+def canny(image):
+    return cv2.Canny(image, 100, 200)
+
+#skew correction
+def deskew(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.bitwise_not(gray)
+    thresh = cv2.threshold(gray, 0, 255,
+        cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    coords = np.column_stack(np.where(thresh > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h),
+        flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)    
+    return rotated
+
+#template matching
+def match_template(image, template):
+    return cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED) 
+
+deskew = deskew(image)
+gray = get_grayscale(deskew)
+thresh = thresholding(gray)
+rnoise = remove_noise(gray)
+dilate = dilate(gray)
+erode = erode(gray)
+opening = opening(gray)
+canny = canny(gray)
+
+def show_images(images, cols = 1, titles = None):
+    assert((titles is None)or (len(images) == len(titles)))
+    n_images = len(images)
+    if titles is None: titles = ['Image (%d)' % i for i in range(1,n_images + 1)]
+    fig = plt.figure()
+    for n, (image, title) in enumerate(zip(images, titles)):
+        a = fig.add_subplot(cols, np.ceil(n_images/float(cols)), n + 1)
+        if image.ndim == 2:
+            plt.gray()
+        plt.imshow(image)
+        a.set_title(title)
+    fig.set_size_inches(np.array(fig.get_size_inches()) * n_images)
+    plt.show()
+
+show_images(images, 3, ["gray","rnoise","dilate","erode","thresh","deskew","opening","canny"])
+
