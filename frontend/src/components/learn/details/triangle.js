f6e9447cbb13c9036bcf181eab47b36e90ca96d2
aman <ajaiswal@cs.iitr.ac.in>
2020-08-03 09:32:15 +0530

    Add learn components details(Triangle)

--- frontend/src/components/learn/details/triangle.js
+++ frontend/src/components/learn/details/triangle.js
@@ -8,12 +8,9 @@ export const Triangle = () => {
         <Segment className="descriptionStyle">
 
 				<h2>I am a 'tri' angle and I have three angles !!</h2>
-				<p2>
-
-				</p2>
-				<p2>
-					
-				</p2>
+				<p>Now after executing basic sequential operation let's now try to make a triangle by our dog's trajectory. You will have to create a equilateral traingle. For creating a equilateral traingle we need to create three same lines of equal length and angle w.r.t to each other. We can do these by writing instructions for each line manually or by using what we call looping construct which executes instructions within it a definite no times. For creating a looping construct in our language we will use repeat and end repeat blocks, instructions inside it will be repeated.</p>
+				<p>Hint:create a straight line of lenght five and rotate by 60 degree at the end by method we covered earlier.Now place those instuction blocks within a repeat and end repeat blocks with 3 times as value for repeat block.</p>
+        
         </Segment>
 	);
 };
