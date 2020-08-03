202fc25f99b359ba18c682920b46534d015a581c
aman <ajaiswal@cs.iitr.ac.in>
2020-08-03 09:45:00 +0530

    Add learn components details(Square)

--- frontend/src/components/learn/details/square.js
+++ frontend/src/components/learn/details/square.js
@@ -9,13 +9,7 @@ export const Square = () => {
 			<h2>The power of symmetry : )</h2>
 			<p2></p2>
 			<p2>
-				Hint: As earlier our instruction blocks will go in between a start and
-				stop block. Put a forward block and times value 4 block to make the dog
-				move forward by 4 step . Now to make the dog turn clockwise we need to
-				form sequence of instruction with forward instruction . This will be
-				done by placing clockwise block after the forward block and also placing
-				a rotate 90 degree block beside that to make a 90 degree turn . Now
-				after this repeat same forward blocks after clockwise block
+			Now this level will be combination of all the techniques we learnt up till now . Try to make a straight line and rotated anticlockwise by 90 degree at the end and repeat this in repeat block 4 times to get a square.
 			</p2>
 		</Segment>
 	);
